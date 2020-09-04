5823d25dc0db2bc1976f1356907c0fd4f719ec25
benma <cmpbmade@ljmu.ac.uk>
2020-09-04 17:05:51 +0100

    Initial project structure and index.php layout.

---
+++ pages/index.php
@@ -0,0 +1,44 @@
+    <!DOCTYPE html>
+    <html lang="en-US">
+
+    <head>
+        <meta charset="UTF-8">
+
+        <!-- PAGE TITLE -->
+        <title>Canvas - Home</title>
+
+        <!-- SITE META DATA -->
+        <meta name="keywords" content="GAME, RECOMMEND, 4122COMP, WEB DEV, HTML,CSS">
+        <meta name="description" content="A Nintendo Switch Game Recommendation website of 10 Ranked titles.">
+        <meta name="author" content="Ben Madelin">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- SITE RESOURCES -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css">
+        <link rel="icon" type="image/png" href="..\img\favicon.png">
+
+        <!-- PAGE STYLING -->
+        <link rel="stylesheet" type="text/css" href="..\style\style.css">
+
+        <style>
+
+
+
+        </style>
+
+    </head>
+
+    <body>
+
+    <!-- HEADER -->
+    <h1>Canvas Challenge</h1>
+
+    <!-- CONTENT -->
+
+    <!-- FOOTER -->
+
+
+
+    </body>
+
+    </html>
