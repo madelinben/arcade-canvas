b0e544ae3a4d252574ba280b833ad6d7db3681e0
benma <cmpbmade@ljmu.ac.uk>
2020-09-05 16:39:38 +0100

    Snake size is incremented when a piece of fruit is eaten, and each tail cell is updated.

--- scripts/snake/snake.js
+++ scripts/snake/snake.js
@@ -3,13 +3,26 @@ function Snake() {
     this.y = 0;
     this.xSpeed = scale;
     this.ySpeed = 0;
+    this.size = 0;
+    this.tail = [];
 
     this.draw = function() {
         ctx.fillStyle = "#071821";
+
+        for (let i=0; i<this.tail.length; i++) {
+            ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+        }
+
         ctx.fillRect(this.x, this.y, scale, scale);
     }
 
     this.update = function() {
+        for (let i=0; i<this.tail.length - 1; i++) {
+            this.tail[i] = this.tail[i+1];
+        }
+
+        this.tail[this.size - 1] = { x: this.x, y: this.y };
+
         this.x += this.xSpeed;
         this.y += this.ySpeed;
 
@@ -53,6 +66,7 @@ function Snake() {
 
     this.eat = function(fruit) {
         if (this.x === fruit.x && this.y === fruit.y) {
+            this.size++;
             return true;
         } else {
             return false;
